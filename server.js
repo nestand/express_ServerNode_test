c45d0e65402f693463616531c5fc9906c6daf537
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-08-07 23:12:48 +0200

    index route added

--- server.js
+++ server.js
@@ -4,5 +4,9 @@
 //'require express' guide from DOC
 const express = require ("express");
  const app = express();
+
+ app.get("/", function(request, response){
+     response.send("Hello, World! It's the TEST!");
+ });
  
  app.listen(3000);
